31ef9c27cc0f8f408cd359c64c3d26143e22038c
BertZZ <bertie.wooles@hotmail.co.uk>
2018-03-02 16:46:42 +0000

    Adds quit function and can respond to certain commands

--- lilybot.rb
+++ lilybot.rb
@@ -29,15 +29,29 @@ class LilyBot
     until @socket.eof? do
       message = @socket.gets
       puts message
+      if message.match(/^PING :(.*)$/)
+        write_to_system "PONG #{$~[1]}"
+        next
+      end
+
       if message.match(/PRIVMSG ##{@channel} :(.*)$/)
         content = $~[1]
-        if content.include? "!about"
+        if content.include? "!commands"
+          write_to_chat("Current commands are: !about")
+        elsif content.include? "!about"
           write_to_chat("This will be some information about Lily who is amazing and awesome")
         end
       end
     end
   end
+
+  def quit
+    write_to_chat "LilyBot Has Crashed"
+    write_to_system "PART ##{@channel}"
+    write_to_system "QUIT"
+  end
 end
 
 lilybot = LilyBot.new
+trap("INT") {lilybot.quit}
 lilybot.run
